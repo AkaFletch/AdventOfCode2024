9305d4c883f4e9d60f461313a828cb71a692f9f9
Matt <matt@threlfall.dev>
2024-12-07 22:53:59 +0000

    Day2 Part1

---
+++ Day2/Part1/src/main.rs
@@ -0,0 +1,58 @@
+use std::fs;
+
+#[derive(Copy, Clone)]
+enum Direction {
+    Starting,
+    Increasing,
+    Decreasing,
+}
+
+fn main() {
+    println!("Day 2 Part 1");
+    let input = fs::read_to_string("input.prod").expect("Failed to read input");
+    let mut safe_report = 0;
+    'report: for report in input.split("\n") {
+        if report.is_empty() {
+            continue;
+        }
+        let list: Vec<i32> = report
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+        let mut last = list[0];
+        let mut dir = Direction::Starting;
+        let mut dif: i32;
+        let mut diff_check: bool;
+        for (i, level) in list.iter().enumerate() {
+            let index_direction = (i, dir.clone());
+            dif = (last - *level).abs();
+            diff_check = dif < 1 || dif > 3;
+            match index_direction {
+                (_, Direction::Increasing) => {
+                    if last >= *level || diff_check {
+                        continue 'report;
+                    }
+                }
+                (_, Direction::Decreasing) => {
+                    if *level >= last || diff_check {
+                        continue 'report;
+                    }
+                }
+                (1, _) => {
+                    if diff_check {
+                        continue 'report;
+                    }
+                    dir = if last < *level {
+                        Direction::Increasing
+                    } else {
+                        Direction::Decreasing
+                    };
+                }
+                (_, Direction::Starting) => {}
+            }
+            last = *level;
+        }
+        safe_report += 1;
+    }
+    println!("Safe reports {}", safe_report);
+}
